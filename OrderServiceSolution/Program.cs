33dd52160a69d15b36dfe62f4d310bca94215902
aparna60 <abhattacharya60@gmail.com>
2025-02-20 12:55:03 +0530

    Integrated advanced Polly configurations i.e fallbackPolicy,retryPolicy, circuitBreakerPolicy, timeoutPolicy, bulkHeadPolicy

--- OrderServiceSolution/Program.cs
+++ OrderServiceSolution/Program.cs
@@ -1,4 +1,23 @@
 using Polly;
+using Polly.Extensions.Http;
+
+var retryPolicy= HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .WaitAndRetryAsync(3, retryAttempt=> TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
+
+var circuitBreakerPolicy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .CircuitBreakerAsync(3, TimeSpan.FromSeconds(30));
+
+var timeoutPolicy = Policy.TimeoutAsync<HttpResponseMessage>(5);
+
+var bulkHeadPolicy = Policy.BulkheadAsync<HttpResponseMessage>(10, 20);
+
+var fallbackPolicy = Policy<HttpResponseMessage>.Handle<HttpRequestException>()
+    .FallbackAsync(new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+    {
+        Content = new StringContent("[{\"ProductId\":0,\"Name\":\"Default Product\",\"Description\":\"Service Unavailable\"}]")
+    });
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +29,11 @@ builder.Services.AddHttpClient("ProductService",
     client =>
     {
         client.BaseAddress = new Uri("http://localhost:5125/api/product");
-    }).AddTransientHttpErrorPolicy(policy=> policy.WaitAndRetryAsync(3, retryattempt => TimeSpan.FromSeconds(retryattempt)));
-   
+    }).AddPolicyHandler(fallbackPolicy)                     // Provide a default response when the API is down
+    .AddPolicyHandler(retryPolicy)                          // Retry on failures
+    .AddPolicyHandler(circuitBreakerPolicy)                 // Stop calling failing services temporarily
+    .AddPolicyHandler(timeoutPolicy)                        // Cancel long-running requests
+    .AddPolicyHandler(bulkHeadPolicy);                      // Limit concurrent requests
 
 var app = builder.Build();
 
